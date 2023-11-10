b46f254f2f2e956e3480c19260f069a903d6a643
Mac Sweeny <macsweenydev@gmail.com>
2023-11-10 11:14:13 +1000

    fix: updated SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import "./globals.css";
 import type {Metadata} from "next";
 
 export const metadata: Metadata = {
-  title: "Mac Sweeny | Portfolio",
-  description: "Welcome to my portfolio!",
+  title: "Mac Sweeny",
+  description:
+    "G'day, I'm Mac Sweeny, a Full-Stack Web Developer from Australia. This is my personal website where I write about my projects, tips and tricks, and other things I find interesting. I'm currently working on a few projects such as a website builder using the new Payload 2.0 CMS, a new version of my personal blog, and a few other things.",
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
